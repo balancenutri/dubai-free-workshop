a3832ec623673a5dea8bdfa0dc88ff47196999d1
KundanChaudharyBN <Kundan.Chaudhary@balancenutrition.in>
2025-01-15 21:04:04 +0530

    update name of the venue on congrates page

--- src/page/Congrats.jsx
+++ src/page/Congrats.jsx
@@ -55,7 +55,7 @@ function App() {
               </div>
               <div>
                 <p className="md:text-base text-gray-800 font-semibold">
-                  India Club, Dubai
+                  Dubai
                 </p>
               </div>
             </div>
