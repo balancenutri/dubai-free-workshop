bfa88bb0b2195bbbf8451b17e4c0f29c7f6c2f48
KundanChaudharyBN <Kundan.Chaudhary@balancenutrition.in>
2025-01-24 19:10:26 +0530

    update the card as per the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
 import Congrats from "./page/Congrats";
+import Test from "./page/Test";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/congrats" element={<Congrats />} />
+        <Route path="/" element={<Test />} />
+        {/* <Route path="/congrats" element={<Congrats />} /> */}
       </Routes>
     </Router>
   )
